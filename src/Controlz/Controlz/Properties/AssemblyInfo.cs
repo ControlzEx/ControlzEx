60eb353aa53f85bc57f4ff3a46dddcb45838a382
Jan Karger <punkerat76@gmail.com>
2015-10-11 00:54:35 +0200

    v1.0.0

--- src/Controlz/Controlz/Properties/AssemblyInfo.cs
+++ src/Controlz/Controlz/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyProduct("Controlz 1.0.0-alpha001")]
+[assembly: AssemblyProduct("Controlz 1.0.0")]
