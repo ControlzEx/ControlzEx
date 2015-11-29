777bb0453d7f2a729197db75cf081bdf5c97e52f
Bastian Schmidt <batzen@gmx.org>
2015-11-29 11:02:15 +0100

    Adding GlobalAssemblyInfo

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("https://github.com/ControlzEx/ControlzEx")]
+[assembly: AssemblyProduct("ControlzEx")]
+[assembly: AssemblyCopyright("Copyright © 2015 Jan Karger, Bastian Schmidt")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("SRC")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
