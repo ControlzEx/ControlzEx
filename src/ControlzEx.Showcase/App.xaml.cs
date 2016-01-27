313937768824e00f1d0d1d6700918ae0af166d2c
Bastian Schmidt <batzen@gmx.org>
2016-01-27 17:44:45 +0100

    File cleanup

--- src/ControlzEx.Showcase/App.xaml.cs
+++ src/ControlzEx.Showcase/App.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace ControlzEx.Showcase
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+        }
     }
 }
