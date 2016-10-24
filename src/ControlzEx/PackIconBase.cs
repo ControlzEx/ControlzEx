37f0dfd85726724478eb87f1d2808cf8376fde92
Jan Karger <punkerat76@gmail.com>
2016-10-24 22:40:31 +0200

    Prepare PackIconBase for UWP usage

--- src/ControlzEx/PackIconBase.cs
+++ src/ControlzEx/PackIconBase.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Collections.Generic;
+#if NETFX_CORE
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+#else
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+#endif
 
 namespace ControlzEx
 {
@@ -49,23 +54,34 @@ namespace ControlzEx
             set { SetValue(KindProperty, value); }
         }
 
+#if NETFX_CORE
+        private static readonly DependencyProperty DataProperty
+            = DependencyProperty.Register(nameof(Data), typeof(string), typeof(PackIconBase<TKind>), new PropertyMetadata(""));
+#else
         private static readonly DependencyPropertyKey DataPropertyKey
             = DependencyProperty.RegisterReadOnly(nameof(Data), typeof(string), typeof(PackIconBase<TKind>), new PropertyMetadata(""));
 
         // ReSharper disable once StaticMemberInGenericType
         public static readonly DependencyProperty DataProperty = DataPropertyKey.DependencyProperty;
+#endif
 
         /// <summary>
         /// Gets the icon path data for the current <see cref="Kind"/>.
         /// </summary>
+#if !NETFX_CORE
         [TypeConverter(typeof(GeometryConverter))]
+#endif
         public string Data
         {
             get { return (string)GetValue(DataProperty); }
-            private set { SetValue(DataPropertyKey, value); }
+            private set { SetValue(DataProperty, value); }
         }
 
+#if NETFX_CORE
+        protected override void OnApplyTemplate()
+#else
         public override void OnApplyTemplate()
+#endif
         {
             base.OnApplyTemplate();
 
