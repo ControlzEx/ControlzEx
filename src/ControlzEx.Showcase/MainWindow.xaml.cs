0bb95c7c5b1f6e98d9f641ee57bd9728652bbada
Bastian Schmidt <batzen@gmx.org>
2017-05-23 18:46:06 +0200

    Improving showcase

--- src/ControlzEx.Showcase/MainWindow.xaml.cs
+++ src/ControlzEx.Showcase/MainWindow.xaml.cs
@@ -1,12 +1,17 @@
-﻿using System.Windows;
-
-namespace ControlzEx.Showcase
+﻿namespace ControlzEx.Showcase
 {
+    using System.Windows.Input;
+
     public partial class MainWindow
     {
         public MainWindow()
         {
             this.InitializeComponent();
+        }
+
+        private void TitleBarGrid_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
         }
     }
 }
