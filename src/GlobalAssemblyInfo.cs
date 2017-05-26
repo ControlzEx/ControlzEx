a5c8ef3e3f18c93768f67bb991b90b04230caf41
Bastian Schmidt <batzen@gmx.org>
2017-05-26 21:22:24 +0200

    Setting CLSCompliant to false

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Windows;
 [assembly: AssemblyInformationalVersion("SRC")]
 
 [assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
+[assembly: CLSCompliant(false)]
 
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
