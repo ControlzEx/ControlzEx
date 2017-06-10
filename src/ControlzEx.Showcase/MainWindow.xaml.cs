12852a78c4e836967a9623f5b9a1beb6dba4a087
Bastian Schmidt <batzen@gmx.org>
2017-06-10 23:28:05 +0200

    Improving showcase application

--- src/ControlzEx.Showcase/MainWindow.xaml.cs
+++ src/ControlzEx.Showcase/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿namespace ControlzEx.Showcase
 {
     using System.Windows.Input;
+#pragma warning disable 618
+    using SystemCommands = Microsoft.Windows.Shell.SystemCommands;
+#pragma warning restore 618
 
     public partial class MainWindow
     {
@@ -12,6 +15,13 @@
         private void TitleBarGrid_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
+
+        private void TitleBarGrid_OnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+#pragma warning disable 618
+            SystemCommands.ShowSystemMenu(this, e);
+#pragma warning restore 618
         }
     }
 }
