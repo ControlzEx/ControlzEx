929e12fe966ea3eca3bd322c6634cc5bd10e7422
Bastian Schmidt <batzen@gmx.org>
2017-08-25 19:03:13 +0200

    Improving window movement in showcase

--- src/ControlzEx.Showcase/MainWindow.xaml.cs
+++ src/ControlzEx.Showcase/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
 ﻿namespace ControlzEx.Showcase
 {
+    using System;
+    using System.Reflection;
+    using System.Windows;
     using System.Windows.Input;
+    using ControlzEx.Native;
+    using Standard;
 #pragma warning disable 618
     using SystemCommands = Microsoft.Windows.Shell.SystemCommands;
 #pragma warning restore 618
@@ -12,9 +17,36 @@
             this.InitializeComponent();
         }
 
+        private static readonly PropertyInfo criticalHandlePropertyInfo = typeof(Window).GetProperty("CriticalHandle", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly object[] emptyObjectArray = new object[0];
+
         private void TitleBarGrid_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            this.DragMove();
+            if (e.ClickCount == 1)
+            {
+                e.Handled = true;
+
+                // taken from DragMove internal code
+                this.VerifyAccess();
+
+                // for the touch usage
+#pragma warning disable 618
+                UnsafeNativeMethods.ReleaseCapture();
+
+                var criticalHandle = (IntPtr)criticalHandlePropertyInfo.GetValue(this, emptyObjectArray);
+                // DragMove works too, but not on maximized windows
+                NativeMethods.SendMessage(criticalHandle, WM.SYSCOMMAND, (IntPtr)SC.MOUSEMOVE, IntPtr.Zero);
+                NativeMethods.SendMessage(criticalHandle, WM.LBUTTONUP, IntPtr.Zero, IntPtr.Zero);
+#pragma warning restore 618
+            }
+            else if (e.ClickCount == 2
+                     && this.ResizeMode != ResizeMode.NoResize)
+            {
+                e.Handled = true;
+                this.WindowState = this.WindowState == WindowState.Maximized
+                                         ? WindowState.Normal
+                                         : WindowState.Maximized;
+            }
         }
 
         private void TitleBarGrid_OnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
