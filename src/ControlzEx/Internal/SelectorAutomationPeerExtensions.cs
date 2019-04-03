cb1edada4a04b81b806fa48f770bef0aa4890640
Bastian Schmidt <batzen@gmx.org>
2019-04-03 19:06:43 +0200

    Adding some helper classes to get access to framework internal methods

---
+++ src/ControlzEx/Internal/SelectorAutomationPeerExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace ControlzEx.Internal
+{
+    using System.Reflection;
+    using System.Windows.Automation.Peers;
+    using System.Windows.Controls;
+
+    internal static class SelectorAutomationPeerExtensions
+    {
+        private static readonly MethodInfo RaiseSelectionEventsMethodInfo = typeof(SelectorAutomationPeer).GetMethod("RaiseSelectionEventsMethodInfo", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        internal static void RaiseSelectionEvents(this SelectorAutomationPeer selectorAutomationPeer, SelectionChangedEventArgs e)
+        {
+            RaiseSelectionEventsMethodInfo.Invoke(selectorAutomationPeer, new[]
+                                                                          {
+                                                                              e
+                                                                          });
+        }
+    }
+}
---
+++ src/ControlzEx/Internal/TabItemExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace ControlzEx.Internal
+{
+    using System.Reflection;
+    using System.Windows.Controls;
+
+    internal static class TabItemExtensions
+    {
+        private static readonly MethodInfo SetFocusMethodInfo = typeof(TabItem).GetMethod("SetFocus", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        internal static bool SetFocus(this TabItem tabItem)
+        {
+            return (bool)SetFocusMethodInfo.Invoke(tabItem, null);
+        }
+    }
+}
