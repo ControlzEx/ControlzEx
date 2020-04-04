6b0c35a35a60e1bf126731f87e176071666282f4
Bastian Schmidt <batzen@gmx.org>
2020-04-04 14:28:32 +0200

    Adding a few more tests

---
+++ src/ControlzEx.Tests/Theming/RuntimeThemeGeneratorTests.cs
@@ -0,0 +1,15 @@
+﻿namespace ControlzEx.Tests.Theming
+{
+    using ControlzEx.Theming;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class RuntimeThemeGeneratorTests
+    {
+        [Test]
+        public void Current_Should_Not_Be_Null()
+        {
+            Assert.That(RuntimeThemeGenerator.Current, Is.Not.Null);
+        }
+    }
+}
