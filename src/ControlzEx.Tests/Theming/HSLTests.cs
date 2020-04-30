b15976ae63716b01f74d55d31e7bc6090d8925dd
Tim <tim.uniewski@gmail.com>
2020-04-30 11:38:39 +0200

    Implement some Tests for the HSL-Struct

---
+++ src/ControlzEx.Tests/Theming/HSLTests.cs
@@ -0,0 +1,112 @@
+﻿using ControlzEx.Theming;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ControlzEx.Tests.Theming
+{
+    [TestFixture]
+    public class HSLTests
+    {
+        // This Test is very slow, so only run if really needed. Comment out the comment below to run this test
+        // [Test]
+        public void TestHslFromColor()
+        {
+            for (uint i = 0; i < uint.MaxValue; i++)
+            {
+                var color = (Color)ColorConverter.ConvertFromString("#" + i.ToString("X8"));
+
+                Assert.AreEqual(color, new HSLColor(color).ToColor());
+            }
+        }
+
+        [Test]
+        public void TestColorPerComponent()
+        {
+            Color color;
+
+            // Gray
+            for (byte i = 0; i < byte.MaxValue; i++)
+            {
+                color = Color.FromArgb(255, i, i, i);
+                Assert.AreEqual(color, new HSLColor(color).ToColor());
+            }
+
+            // A
+            for (byte i = 0; i < byte.MaxValue; i++)
+            {
+                color = Color.FromArgb(i, 255, 255, 255);
+                Assert.AreEqual(color, new HSLColor(color).ToColor());
+            }
+            
+            // R
+            for (byte i = 0; i < byte.MaxValue; i++)
+            {
+                color = Color.FromArgb(255, i, 255, 255);
+                Assert.AreEqual(color, new HSLColor(color).ToColor());
+            }
+
+            // G
+            for (byte i = 0; i < byte.MaxValue; i++)
+            {
+                color = Color.FromArgb(255, 255, i, 255);
+                Assert.AreEqual(color, new HSLColor(color).ToColor());
+            }
+
+            // B
+            for (byte i = 0; i < byte.MaxValue; i++)
+            {
+                color = Color.FromArgb(255, 255, 255, i);
+                Assert.AreEqual(color, new HSLColor(color).ToColor());
+            }
+        }
+
+        [Test]
+        public void TestHslFromColor_BuildInColors()
+        {
+            foreach (var color in typeof(Colors).GetProperties().Where(x => x.PropertyType == typeof(Color)).Select(x => (Color)x.GetValue(null)))
+            {
+                Assert.AreEqual(color, new HSLColor(color).ToColor());
+            }
+        }
+
+
+        [Test]
+        public void TestHslFromInput()
+        {
+            // Transparent
+            Assert.AreEqual(Colors.Transparent, new HSLColor(0, 0, 0, 1).ToColor());
+            
+            // Black
+            Assert.AreEqual(Colors.Black, new HSLColor(1, 0, 0, 0).ToColor());
+
+            // White
+            Assert.AreEqual(Colors.White, new HSLColor(1, 0, 0, 1).ToColor());
+            
+            // Gray
+            Assert.AreEqual(Colors.Gray, new HSLColor(1, 0, 0, 0.5).ToColor());
+
+            // Red
+            Assert.AreEqual(Colors.Red, new HSLColor(1, 0, 1, 0.5).ToColor());
+
+            // Yellow
+            Assert.AreEqual(Colors.Yellow, new HSLColor(1, 60, 1, 0.5).ToColor());
+
+            // Lime (Green)
+            Assert.AreEqual(Colors.Lime, new HSLColor(1, 120, 1, 0.5).ToColor());
+
+            // Aqua
+            Assert.AreEqual(Colors.Aqua, new HSLColor(1, 180, 1, 0.5).ToColor());
+
+            // Blue
+            Assert.AreEqual(Colors.Blue, new HSLColor(1, 240, 1, 0.5).ToColor());
+
+            // Magenta
+            Assert.AreEqual(Colors.Magenta, new HSLColor(1, 300, 1, 0.5).ToColor());
+        }
+    }
+}
