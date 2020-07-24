d8c4185a10761b644a1821fc2c721ef772ca32b5
punker76 <punkerat76@gmail.com>
2020-07-24 17:12:16 +0200

    Add GetWindowText, GetWindowTextLength, Make error accessible at Win32Error

--- src/ControlzEx/Microsoft.Windows.Shell/Standard/NativeMethods.cs
+++ src/ControlzEx/Microsoft.Windows.Shell/Standard/NativeMethods.cs
@@ -3725,6 +3725,44 @@ namespace ControlzEx.Standard
             return rc;
         }
 
+        /// <summary>
+        /// Retrieves the length, in characters, of the specified window's title bar text (if the window has a title bar).
+        /// If the specified window is a control, the function retrieves the length of the text within the control. However,
+        /// GetWindowTextLength cannot retrieve the length of the text of an edit control in another application.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window or control.</param>
+        /// <returns>
+        /// If the function succeeds, the return value is the length, in characters, of the text. Under certain
+        /// conditions, this value may actually be greater than the length of the text. For more information, see the following
+        /// Remarks section.
+        /// <para>If the window has no text, the return value is zero. To get extended error information, call GetLastError.</para>
+        /// </returns>
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+        /// <summary>
+        /// Copies the text of the specified window's title bar (if it has one) into a buffer. If the specified window is
+        /// a control, the text of the control is copied. However, GetWindowText cannot retrieve the text of a control in another
+        /// application.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window or control containing the text.</param>
+        /// <param name="strText">
+        /// The buffer that will receive the text. If the string is as long or longer than the buffer, the
+        /// string is truncated and terminated with a null character.
+        /// </param>
+        /// <param name="maxCount">
+        /// The maximum number of characters to copy to the buffer, including the null character. If the
+        /// text exceeds this limit, it is truncated.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is the length, in characters, of the copied string, not including
+        /// the terminating null character. If the window has no title bar or text, if the title bar is empty, or if the window or
+        /// control handle is invalid, the return value is zero. To get extended error information, call GetLastError.
+        /// <para>This function cannot retrieve the text of an edit control in another application.</para>
+        /// </returns>
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder strText, int maxCount);
+
         [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         [DllImport("gdiplus.dll")]
         public static extern Status GdipCreateBitmapFromStream(IStream stream, out IntPtr bitmap);
