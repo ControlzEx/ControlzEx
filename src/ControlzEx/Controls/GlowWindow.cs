89987370f74193dcf1e3af91e0560a3844cd3595
Bastian Schmidt <batzen@gmx.org>
2021-10-21 10:08:33 +0200

    Moving ChangeScope to separate file

--- src/ControlzEx/Controls/GlowWindow.cs
+++ src/ControlzEx/Controls/GlowWindow.cs
@@ -150,26 +150,6 @@ namespace ControlzEx.Controls.Internal
         }
     }
 
-    public sealed class ChangeScope : DisposableObject
-    {
-        private readonly GlowWindowBehavior behavior;
-
-        public ChangeScope(GlowWindowBehavior behavior)
-        {
-            this.behavior = behavior;
-            this.behavior.DeferGlowChangesCount++;
-        }
-
-        protected override void DisposeManagedResources()
-        {
-            this.behavior.DeferGlowChangesCount--;
-            if (this.behavior.DeferGlowChangesCount == 0)
-            {
-                this.behavior.EndDeferGlowChanges();
-            }
-        }
-    }
-
     [PublicAPI]
     public class DisposableObject : IDisposable
     {
