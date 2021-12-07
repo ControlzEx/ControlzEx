57e9c8428e23c4f0264fe7eae4bf19ef32db0144
Bastian Schmidt <batzen@gmx.org>
2021-12-07 16:58:18 +0100

    Adding native TrackMouseEvent things

--- src/ControlzEx/Microsoft.Windows.Shell/Standard/NativeMethods.cs
+++ src/ControlzEx/Microsoft.Windows.Shell/Standard/NativeMethods.cs
@@ -4377,7 +4377,66 @@ namespace ControlzEx.Standard
                 return num;
             }
         }
-        
+
+        [CLSCompliant(false)]
+        [DllImport("user32.dll", SetLastError=true)]
+        public static extern bool TrackMouseEvent(ref TRACKMOUSEEVENT lpEventTrack);
+
+        [CLSCompliant(false)]
+        [StructLayout(LayoutKind.Sequential)]
+        public struct TRACKMOUSEEVENT
+        {
+            public int cbSize;    // using Int32 instead of UInt32 is safe here, and this avoids casting the result  of Marshal.SizeOf()
+            [MarshalAs(UnmanagedType.U4)]
+            public TMEFlags dwFlags;
+            public IntPtr hWnd;
+            public uint dwHoverTime;
+
+            public TRACKMOUSEEVENT(TMEFlags dwFlags, IntPtr hWnd, uint dwHoverTime)
+            {
+                this.cbSize = Marshal.SizeOf(typeof(TRACKMOUSEEVENT));
+                this.dwFlags = dwFlags;
+                this.hWnd = hWnd;
+                this.dwHoverTime = dwHoverTime;
+            }
+        }
+
+        /// <summary>
+        /// The services requested. This member can be a combination of the following values.
+        /// </summary>
+        // <seealso cref="http://msdn.microsoft.com/en-us/library/ms645604%28v=vs.85%29.aspx"/>
+        [CLSCompliant(false)]
+        [Flags]
+        public enum TMEFlags : uint
+        {
+            /// <summary>
+            /// The caller wants to cancel a prior tracking request. The caller should also specify the type of tracking that it wants to cancel. For example, to cancel hover tracking, the caller must pass the TME_CANCEL and TME_HOVER flags.
+            /// </summary>
+            TME_CANCEL = 0x80000000,
+
+            /// <summary>
+            /// The caller wants hover notification. Notification is delivered as a WM_MOUSEHOVER message.
+            /// If the caller requests hover tracking while hover tracking is already active, the hover timer will be reset.
+            /// This flag is ignored if the mouse pointer is not over the specified window or area.
+            /// </summary>
+            TME_HOVER = 0x00000001,
+
+            /// <summary>
+            /// The caller wants leave notification. Notification is delivered as a WM_MOUSELEAVE message. If the mouse is not over the specified window or area, a leave notification is generated immediately and no further tracking is performed.
+            /// </summary>
+            TME_LEAVE = 0x00000002,
+
+            /// <summary>
+            /// The caller wants hover and leave notification for the nonclient areas. Notification is delivered as WM_NCMOUSEHOVER and WM_NCMOUSELEAVE messages.
+            /// </summary>
+            TME_NONCLIENT = 0x00000010,
+
+            /// <summary>
+            /// The function fills in the structure instead of treating it as a tracking request. The structure is filled such that had that structure been passed to TrackMouseEvent, it would generate the current tracking. The only anomaly is that the hover time-out returned is always the actual time-out and not HOVER_DEFAULT, if HOVER_DEFAULT was specified during the original TrackMouseEvent request.
+            /// </summary>
+            TME_QUERY = 0x40000000,
+        }
+
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool ShowWindow(IntPtr hwnd, SW nCmdShow);
