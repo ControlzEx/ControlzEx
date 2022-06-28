810a9c640359456696863962c3a4dfe33dfb6d1b
Jan Ivar Z. Carlsen <jizc@outlook.com>
2022-06-28 12:51:55 +0200

    Tweaked test

--- src/ControlzEx.Tests/Native/NativeExtensionTests.cs
+++ src/ControlzEx.Tests/Native/NativeExtensionTests.cs
@@ -11,38 +11,40 @@
         [Test]
         public void RectConvertedFromMonitorPositionIsCorrectSize()
         {
+            const int WIDTH = 800;
+            const int HEIGHT = 600;
             var windowPos1 = new WINDOWPOS
             {
                 x = 0,
                 y = 0,
-                cx = 800,
-                cy = 600
+                cx = WIDTH,
+                cy = HEIGHT
             };
             var windowPos2 = new WINDOWPOS
             {
                 x = -500,
                 y = -500,
-                cx = 800,
-                cy = 600
+                cx = WIDTH,
+                cy = HEIGHT
             };
             var windowPos3 = new WINDOWPOS
             {
                 x = 5000,
                 y = 5000,
-                cx = 800,
-                cy = 600
+                cx = WIDTH,
+                cy = HEIGHT
             };
 
             var rect1 = windowPos1.ToRECT();
             var rect2 = windowPos2.ToRECT();
             var rect3 = windowPos3.ToRECT();
 
-            Assert.That(rect1.GetWidth(), Is.EqualTo(800));
-            Assert.That(rect1.GetHeight(), Is.EqualTo(600));
-            Assert.That(rect2.GetWidth(), Is.EqualTo(800));
-            Assert.That(rect2.GetHeight(), Is.EqualTo(600));
-            Assert.That(rect3.GetWidth(), Is.EqualTo(800));
-            Assert.That(rect3.GetHeight(), Is.EqualTo(600));
+            Assert.That(rect1.GetWidth(), Is.EqualTo(WIDTH));
+            Assert.That(rect1.GetHeight(), Is.EqualTo(HEIGHT));
+            Assert.That(rect2.GetWidth(), Is.EqualTo(WIDTH));
+            Assert.That(rect2.GetHeight(), Is.EqualTo(HEIGHT));
+            Assert.That(rect3.GetWidth(), Is.EqualTo(WIDTH));
+            Assert.That(rect3.GetHeight(), Is.EqualTo(HEIGHT));
         }
     }
 }
